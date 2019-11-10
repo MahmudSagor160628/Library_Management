cbbb76d44b626be28da71f4d4b612684e2a06c2f
Mahmud_Sagor <mahmudsagor.ice16.pust@gmail.com>
2019-11-09 23:19:00 -1200

    asset sign in and resgestration created

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+
+    <title>Library Management</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/vendor/bootstrap/css/bootstrap.css">
+
+  </head>
+  <body>
+    <h2 class="text-center">Welcome to Our Library Management System!<h2>
+
+    <div class="container">
+		<div class="row">
+			<div class="col-sm-4 col-sm-offset-4">
+				<a class="btn btn-primary btn-block" href="student/">Student</a>
+				<a class="btn btn-primary btn-block" href="Librarian/">Librarian</a>
+			</div>
+		</div>
+	</div>
+
+  
+  </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+	
